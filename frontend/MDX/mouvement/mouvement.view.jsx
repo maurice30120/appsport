570bd8bf9bf12bebbf5274893933c2091d01a241
d.huyet@coperbee.fr <d.huyet@coperbee.fr>
2022-02-13 22:56:10 +0100

    wip

--- frontend/MDX/mouvement/mouvement.view.jsx
+++ frontend/MDX/mouvement/mouvement.view.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-const Yop = styled.h1`
-  font-size: 50px;
+const Heading = styled.h1`
+	font-size: 50px;
 
-  color: black
-`
-
+	color: black;
+`;
 
 export const MouvementView = ({ data, objectif = 0, rest = 0 }) => {
 	const { Title, content, images, slug } = data;
@@ -16,11 +15,7 @@ export const MouvementView = ({ data, objectif = 0, rest = 0 }) => {
 		<div>
 			<h2>{Title}</h2>
 			<ReactMarkdown source={content} escapeHtml={false} />
-			<Yop>Hello World!</Yop>
-			{/* <Wrapper>
-				<Title>Hello World!</Title>
-			</Wrapper> */}
+			<Heading>Hello World!</Heading>
 		</div>
 	);
 };
-
